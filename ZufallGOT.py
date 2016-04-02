6a326bd9bc0e97bfc8e9f1fcfcf9a59b38e72a21
Anna Bertsch <annabertsch98@gmail.com>
2016-04-02 16:08:09 +0200

    Inital Commit

---
+++ ZufallGOT.py
@@ -0,0 +1,16 @@
+import random
+characters = ['Cersei','Jon Schnee','Jaime', 'Arya','Sansa','Kleinfinger','Daenerys','Tyrion','Margaery']
+textbausteine = [ 'ist ein Kind von','stirbt.','kriegt bald ein Kind.', 'ist in Wahrheit ein Tagaryen.']
+
+chara = random.randint(0,8)
+charakter = characters[chara]
+charazwei = random.randint(0,8)
+charakterzwei = characters[charazwei]
+text = random.randint(0,3)
+textbaustein = textbausteine[text]
+if text == 0:
+    print(charakter + ' ' + textbaustein + ' '+ charakterzwei)
+else:
+    print(charakter + ' ' + textbaustein)
+    
+    
