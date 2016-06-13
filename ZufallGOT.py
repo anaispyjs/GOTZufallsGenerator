a65d680dd0a10c0731787e36928d76d1c7ea11be
pajowu <github@ca.pajowu.de>
2016-06-13 10:31:10 +0300

    refractor ZufallGOT.py, add coveralls travis

--- ZufallGOT.py
+++ ZufallGOT.py
@@ -1,18 +1,28 @@
 import random
 import json
-with open("characters.json") as characterfile:
-	characters = json.load(characterfile)
-textbausteine = [ 'ist ein Kind von','stirbt.','kriegt bald ein Kind.', 'ist in Wahrheit ein Tagaryen.']
+def load_characters(filename = "characters.json"):
+	with open(filename) as characterfile:
+		characters = json.load(characterfile)
+	return characters
 
 
-charakter = random.choice(characters)
-charakterzwei = random.choice(characters)
-textbaustein = random.choice(textbausteine)
+def get_text_blocks():
+	text_blocks = [ 'ist ein Kind von','stirbt.','kriegt bald ein Kind.', 'ist in Wahrheit ein Tagaryen.']
+	return text_blocks
 
 
-if textbaustein[-1] != ".":
-    print("{0} {1} {2}.".format(charakter, textbaustein, charakterzwei))
-else:
-    print("{0} {1}".format(charakter, textbaustein))
-    
-    
+def generate_spoiler(characters, text_blocks):
+	charakter = random.choice(characters)
+	charakterzwei = random.choice(characters)
+	textbaustein = random.choice(text_blocks)
+
+	if textbaustein[-1] != ".":
+	    print("{0} {1} {2}.".format(charakter, textbaustein, charakterzwei))
+	else:
+	    print("{0} {1}".format(charakter, textbaustein))
+
+	return generate_spoiler
+
+
+if __name__ == "__main__":
+	generate_spoiler(load_characters(), get_text_blocks())
