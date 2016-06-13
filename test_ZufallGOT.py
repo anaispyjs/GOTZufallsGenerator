60db2f8bd574fa15d1c5e8c373bb7a869c0c8f76
pajowu <github@ca.pajowu.de>
2016-06-13 12:13:09 +0300

    also test for nonexistant file

--- test_ZufallGOT.py
+++ test_ZufallGOT.py
@@ -3,7 +3,8 @@ from hypothesis.strategies import text, lists, sampled_from
 from ZufallGOT import load_characters, generate_spoiler, get_str_if_unicode
 
 
-@given(sampled_from(["characters.json", "characters.py", "characters.txt"]))
+@given(sampled_from(["characters.json", "characters.py",
+                    "characters.txt", '']))
 def test_load_characters(filename):
     try:
         characters = load_characters(filename)
